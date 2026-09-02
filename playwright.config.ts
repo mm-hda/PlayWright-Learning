321ae2fe90c5bc4c4af9fd83e43e70c535d14a8e
mm-hda <hda@mm-software.com>
2026-09-02 17:19:17 +0530

    for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,18 +4,22 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   reporter: 'html',
+
   timeout: 120000,
+
   expect: {
     timeout: 120000,
   },
+
   workers: 5,
-  retries: 2,
+  retries: 0,
+
   use: {
     baseURL: 'https://practicesoftwaretesting.com',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: true,
+    headless: false,
 
     launchOptions: {
       slowMo: 1500,
@@ -27,7 +31,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-      }
+      },
     },
-  ]
+  ],
 });
